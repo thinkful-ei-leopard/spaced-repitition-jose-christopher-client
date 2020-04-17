a7a0789860d9ab741a488be9e902789dce6e200d
Jose <jozylopez27@gmail.com>
2020-04-17 14:58:22 -0500

    fin

--- src/config.js
+++ src/config.js
@@ -1,8 +1,4 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://mighty-taiga-42387.herokuapp.com/api',
   TOKEN_KEY: 'blogful-client-auth-token',
 }
-
-
-// http://localhost:8000/api
-// https://mighty-taiga-42387.herokuapp.com/api
